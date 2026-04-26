290b62641954b04ecd5fa10a86496f9102c111db
rafaFabian07 <facegamers343@gmail.com>
2026-04-26 15:32:12 +0700

    23 table added

---
+++ app/Http/Controllers/WishlistsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Wishlists;
+use Illuminate\Http\Request;
+
+class WishlistsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Wishlists $wishlists)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Wishlists $wishlists)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Wishlists $wishlists)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Wishlists $wishlists)
+    {
+        //
+    }
+}
