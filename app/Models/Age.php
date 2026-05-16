e140d1b2dee2ff9936fd25426e370841aa7cebf3
rafaFabian07 <facegamers343@gmail.com>
2026-05-16 22:54:34 +0700

    id age fixed

--- app/Models/Age.php
+++ app/Models/Age.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Age extends Model
 {
     protected $table = 'age'; 
-    protected $primaryKey = 'id';
+    protected $primaryKey = 'age_id';
     public $timestamps = false;
 }
